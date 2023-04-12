3b3a7b63c077a87c7bf0aa6f4bcbdf77c1065615
cy <56862963+bcvi@users.noreply.github.com>
2023-04-12 00:06:32 -0400

    this

--- keywords/thisdemo/Car.java
+++ keywords/thisdemo/Car.java
@@ -1,12 +1,17 @@
 package keywords.thisdemo;
 
 /**
- * this can be used to refer current class instance variable.
- * this can be used to invoke current class method (implicitly)
- * this() can be used to invoke current class constructor.
- * this can be passed as an argument in the method call.
- * this can be passed as argument in the constructor call.
- * this can be used to return the current class instance from the method.
+ * 1. as a pointer refer to a instance
+ *      this can be used to refer current class instance variable.
+ *      this can be used to invoke current class method (implicitly)
+ * 
+ * 2. call constructor
+ *      this() can be used to invoke current class constructor.
+ * 
+ * 3. be passed as a parameter (passing an instance)
+ *      this can be passed as an argument in the method call.
+ *      this can be passed as argument in the constructor call.
+ *      this can be used to return the current class instance from the method.
  */
 public class Car {
     public String make;
@@ -53,5 +58,7 @@ public class Car {
         System.out.println(obj.year);
 
         obj.printMakeYear();
+
+        obj.printCls();
     }
 }
